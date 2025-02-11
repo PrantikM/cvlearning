44571089d7994d660a6bbbfe4690e9f1f795a307
PrantikM <145676123+PrantikM@users.noreply.github.com>
2025-02-11 23:26:59 +0530

    rescaling imgs,vids

---
+++ rescale.py
@@ -0,0 +1,17 @@
+import cv2 as cv 
+
+img = cv.imread("D:\github\cvlearning\cat_large.jpg")
+
+def rescaleframe(frame, scale=0.75):
+    width = int(frame.shape[1]*scale)
+    height = int(frame.shape[0]*scale)
+
+    dimensions = (width,height)
+
+    return cv.resize(frame , dimensions , interpolation=cv.INTER_AREA)
+
+resized_image = rescaleframe(img, scale=0.2)
+
+cv.imshow("Img", resized_image)
+
+cv.waitKey(0)
